3bc516f3154fd230f2a1ac4bdbea5a5b943f98f5
Peter P <peter.palocz91@gmail.com>
2018-05-22 12:30:43 +0200

    add poms, docker compose, dockerfile, configservice

---
+++ counterservice/src/main/java/com/example/springbootmicroservices/counterservice/api/CounterController.java
@@ -0,0 +1,16 @@
+package com.example.springbootmicroservices.counterservice.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class CounterController {
+	private static AtomicLong count = new AtomicLong(0);
+
+	@GetMapping(path = "/count")
+	public long getCount(){
+		return count.getAndIncrement();
+	}
+}
